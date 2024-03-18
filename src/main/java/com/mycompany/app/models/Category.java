ed6db5c1e41acc86ba0a72fafc4c03c7d417c49f
Daedma <hdamir163@gmail.com>
2024-03-19 00:52:59 +0400

    Add a models.Category class

---
+++ src/main/java/com/mycompany/app/models/Category.java
@@ -0,0 +1,31 @@
+package com.mycompany.app.models;
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+public class Category {
+	String name;
+	List<ObjectId> goods;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<ObjectId> getGoods() {
+		return goods;
+	}
+
+	public void setGoods(List<ObjectId> goods) {
+		this.goods = goods;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [\n  name=" + name + ",\n  goods=" + goods + "\n]";
+	}
+}
