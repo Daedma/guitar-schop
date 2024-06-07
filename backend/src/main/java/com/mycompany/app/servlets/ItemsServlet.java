95e31fe65129bab2f9068cea81615850d1477c87
Daedma <hdamir163@gmail.com>
2024-06-07 19:58:32 +0400

    Fixes ItemsServlet

--- backend/src/main/java/com/mycompany/app/servlets/ItemsServlet.java
+++ backend/src/main/java/com/mycompany/app/servlets/ItemsServlet.java
@@ -51,11 +51,11 @@ public class ItemsServlet extends BaseServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String uri = req.getRequestURI();
-		if (uri.endsWith("")) { // /api/items
-			searchItems(req, resp);
-		} else if (uri.matches("/api/items/[a-zA-Z0-9]*")) { // /api/items/{id}
+		String pathInfo = req.getPathInfo();
+		if (pathInfo.matches("^/[0-9a-fA-F]{24}$*")) { // /api/items
 			getItemById(req, resp);
+		} else if (pathInfo.endsWith("")) { // /api/items/{id}
+			searchItems(req, resp);
 		} else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
@@ -74,26 +74,30 @@ public class ItemsServlet extends BaseServlet {
 		String type = req.getParameter("type");
 		String query = req.getParameter("q");
 
-		if (type == null || !type.equalsIgnoreCase("guitar") || !type.equalsIgnoreCase("strings")) {
+		if (type == null || !(type.equalsIgnoreCase("guitar") || type.equalsIgnoreCase("strings"))) {
 			writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item type");
 			return;
 		}
 
 		if (type.equalsIgnoreCase("guitar")) {
 			GuitarFilter filter = new GuitarFilter();
-			filter.setSearchQuery(query);
+			if (query != null) {
+				filter.setSearchQuery(query);
+			}
 			List<Guitar> guitars = IterableUtils.toList(daoFactory.createGuitarDAO().findByFilter(filter));
 			writeObject(resp, HttpServletResponse.SC_OK, guitars);
 		} else if (type.equalsIgnoreCase("strings")) {
 			StringsFilter filter = new StringsFilter();
-			filter.setSearchQuery(query);
+			if (query != null) {
+				filter.setSearchQuery(query);
+			}
 			List<Strings> strings = IterableUtils.toList(daoFactory.createStringsDAO().findByFilter(filter));
 			writeObject(resp, HttpServletResponse.SC_OK, strings);
 		}
 	}
 
 	private void getItemById(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		String id = req.getRequestURI().substring("/api/items/".length());
+		String id = req.getPathInfo().substring(1);
 
 		if (id == null || id.isEmpty()) {
 			writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item id");
@@ -101,45 +105,51 @@ public class ItemsServlet extends BaseServlet {
 		}
 
 		Strings strings = daoFactory.createStringsDAO().findById(id);
-		Guitar guitar = daoFactory.createGuitarDAO().findById(id);
-
 		if (strings != null) {
 			writeObject(resp, HttpServletResponse.SC_OK, strings);
-		} else if (guitar != null) {
-			writeObject(resp, HttpServletResponse.SC_OK, guitar);
-		} else {
-			writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Item not found");
+			return;
 		}
+
+		Guitar guitar = daoFactory.createGuitarDAO().findById(id);
+		if (guitar != null) {
+			writeObject(resp, HttpServletResponse.SC_OK, guitar);
+			return;
+		}
+		writeError(resp, HttpServletResponse.SC_NOT_FOUND, "Item not found");
 	}
 
 	private void addNewItem(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		if (!req.getSession().getAttribute("role").equals("admin")) {
+		if (req.getSession().getAttribute("role") == null || !req.getSession().getAttribute("role").equals("admin")) {
 			writeError(resp, HttpServletResponse.SC_FORBIDDEN, "Users can to add new items only with role admin");
-		}
-
-		Good newItem = gson.fromJson(getJsonFromRequest(req), Good.class);
-
-		if (newItem.getType() == null || !newItem.getType().equalsIgnoreCase("guitar")
-				|| !newItem.getType().equalsIgnoreCase("string")) {
-			writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item type");
 			return;
 		}
 
-		if (newItem.getName() == null || newItem.getName().isEmpty()
-				|| newItem.getDescription() == null || newItem.getDescription().isEmpty()
-				|| newItem.getCost() == null || newItem.getImages() == null || newItem.getImages().isEmpty()
-				|| newItem.getRemaining() == null || newItem.getCategories() == null
-				|| newItem.getCategories().isEmpty()) {
-			writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item data");
-			return;
-		}
-
+		List<FileItem> formItems = null;
 		try {
+			formItems = upload.parseRequest(req);
 
-			List<FileItem> formItems = upload.parseRequest(req);
+			String textData = getJsonFromMultipart(formItems);
+
+			Good newItem = gson.fromJson(textData, Good.class);
+
+			if (newItem.getType() == null || !(newItem.getType().equalsIgnoreCase("guitar")
+					|| newItem.getType().equalsIgnoreCase("string"))) {
+				writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item type");
+				return;
+			}
+
+			if (newItem.getName() == null || newItem.getName().isEmpty()
+					|| newItem.getDescription() == null || newItem.getDescription().isEmpty()
+					|| newItem.getCost() == null || newItem.getImages() == null || newItem.getImages().isEmpty()
+					|| newItem.getRemaining() == null || newItem.getCategories() == null
+					|| newItem.getCategories().isEmpty()) {
+				writeError(resp, HttpServletResponse.SC_BAD_REQUEST, "Invalid item data");
+				return;
+			}
+
 			// FIXME спаггети-код (уже поздно...)
 			if (newItem.getType().equalsIgnoreCase("guitar")) {
-				Guitar newGuitar = gson.fromJson(getJsonFromRequest(req), Guitar.class);
+				Guitar newGuitar = gson.fromJson(textData, Guitar.class);
 				String id = daoFactory.createGuitarDAO().save(newGuitar);
 				newGuitar.setId(new ObjectId(id));
 				newGuitar.setImages(saveItemImages(id, formItems));
@@ -150,7 +160,7 @@ public class ItemsServlet extends BaseServlet {
 				daoFactory.createGuitarDAO().update(newGuitar);
 				writeObject(resp, HttpServletResponse.SC_CREATED, newGuitar);
 			} else if (newItem.getType().equalsIgnoreCase("string")) {
-				Strings newStrings = gson.fromJson(getJsonFromRequest(req), Strings.class);
+				Strings newStrings = gson.fromJson(textData, Strings.class);
 				String id = daoFactory.createStringsDAO().save(newStrings);
 				newStrings.setId(new ObjectId(id));
 				newStrings.setImages(saveItemImages(id, formItems));
@@ -170,6 +180,11 @@ public class ItemsServlet extends BaseServlet {
 
 	private List<String> saveItemImages(String itemId, List<FileItem> formItems) throws Exception {
 		String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIRECTORY;
+		// проверка на существование директории и ее создание, если она отсутствует
+		File uploadDir = new File(uploadPath);
+		if (!uploadDir.exists()) {
+			uploadDir.mkdirs();
+		}
 		int imageIndex = 1;
 		List<String> names = new ArrayList<String>();
 		for (FileItem item : formItems) {
@@ -184,4 +199,16 @@ public class ItemsServlet extends BaseServlet {
 		return names;
 	}
 
+	private String getJsonFromMultipart(List<FileItem> formItems) throws IOException {
+		try {
+			for (FileItem fileItem : formItems) {
+				if (fileItem.isFormField()) {
+					return fileItem.getString();
+				}
+			}
+		} catch (Exception e) {
+			throw new IOException(e);
+		}
+		return null;
+	}
 }
